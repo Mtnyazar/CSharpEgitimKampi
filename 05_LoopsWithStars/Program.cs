fe1dd496d8d86f4ffa61e431dc6f268b67ebc70a
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-10-11 18:55:04 +0300

    Revize_4

--- 05_LoopsWithStars/Program.cs
+++ 05_LoopsWithStars/Program.cs
@@ -10,6 +10,191 @@ namespace _05_LoopsWithStars
     {
         static void Main(string[] args)
         {
+            #region Alt Alta 10 Tane Yıldız Oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+            #region Yan Yana 10 tane Yıldız Oluşturma
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+
+
+
+
+
+            #endregion
+
+
+            #region Alt ALta 10 tane Yıldız Oluşturma ve Her Satırda 10 tane Yıldız Olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*************");
+            //}
+
+            #endregion
+
+
+            #region Dik Üçgen
+
+
+            //for (int i = 1;i<=15;i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+            #region Ters Dik Üçgen
+
+            //for (int i = 10; i >= 1; i--) 
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            #endregion
+
+
+            #region Elmas
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for(int i = 4;i>=1;i--)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+
+            #region Baklava Dilimi
+
+            //int n = 5;
+
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    for(int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for(int k=1;k<=2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1;j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <=2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine() ;
+            //}
+
+
+            #endregion
+
+
+            #region Piramit
+
+            //int n = 5;
+
+            //for (int i = 1;i<=n;i++)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+
+            #region Ters Piramit
+
+            //int n = 5;
+
+            //for (int i = n;i>=1;i--)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+
+
+            Console.Read();
         }
     }
 }
