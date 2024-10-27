38086341a7ac49d1b6da10e3a1899c7fa1ddfe48
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-10-27 20:02:35 +0300

    ders yedi

---
+++ 07_ForeachLoops/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreeach Döngüsü
+
+            //string[] cities = { "İstanbul", "Ankara", "Bursa", "Konya", "Artvin","İzmir" };
+
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //};
+
+
+
+
+            #endregion
+
+
+            #region Örenk Sınav Sistemi Uygulaması
+
+            Console.Write("***** C# Eğitim Kampı Sınav Uygulaması *****");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.Write("Sınıfınızda Kaç Öğrenci Var :");
+
+            int studentCount=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------");
+
+
+
+            #endregion
+
+
+
+
+            Console.Read();
+        }
+    }
+}
