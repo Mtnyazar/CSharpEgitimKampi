d73b925b8d2b806bb4af27866630eb7e8f118490
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-10-29 18:08:50 +0300

    ders7

--- 07_ForeachLoops/Program.cs
+++ 07_ForeachLoops/Program.cs
@@ -40,17 +40,64 @@ namespace _07_ForeachLoops
 
             Console.Write("Sınıfınızda Kaç Öğrenci Var :");
 
-            int studentCount=int.Parse(Console.ReadLine());
+             int studentCount = int.Parse(Console.ReadLine());
 
             Console.WriteLine("----------------------------------------");
 
 
+            string[] studentNames = new string[studentCount];
+            double[] studentExamAvg = new double[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write($"{i + 1}.Öğrencinin İsimini Giriniz :");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                //Her Öğrenci için 3 Sınav Notu Girilecek
+
+
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{studentNames[i]} isimli öğrencinin {j + 1}. sınav notunu giriniz :");
+                    double value = double.Parse(Console.ReadLine());
+
+                    totalExamResult += value;
+                    Console.WriteLine("---------------------------------");
+                }
+                Console.WriteLine();
+
+                studentExamAvg[i] = totalExamResult/3;
+                
+            }
+
+            //Öğrencilerin Ortalaması VE Geçip Kalma Durumları
+
+            Console.WriteLine("**** Öğrenci Durumları ******");
+            Console.WriteLine();
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine($"{studentNames[i]} adlı Öğrencinin Ortalaması :{studentExamAvg[i]}");  
+
+                //Öğrencilerin Ortalaması ve Geçip Kalma Durumları
+
+                if ( studentExamAvg[i] >=50)
+                {
+                    Console.WriteLine($"{studentNames[i]} isimli Öğrenci Dersi Geçti.");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} isimli Öğrenci Dersten Kaldı");
+                    Console.Beep();
+                }
+                Console.WriteLine("--------------------------------------") ;
+            }
 
             #endregion
-
-
-
-
             Console.Read();
         }
     }
