f0f489bf9e225185b0b0f7722d9a34945e99c29e
Mtnyazar <94050730+Mtnyazar@users.noreply.github.com>
2024-11-01 18:17:05 +0300

    DErs_8

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void "Geriye Değer Döndürmeyen" Metotlar
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Metin Yazar");
+            //    Console.WriteLine("Mehtap Yazar");
+            //    Console.WriteLine("Miray Yazar");
+            //    Console.WriteLine("Ahmet Kaan Yazar");
+            //};
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+
+
+            //string CountryCard(string countryName,string capital,string flagColor)
+            //{
+            //    string cardInfo = "-Ülke: " + countryName + " - Başkent: " + capital + "- Bayrak Rengi: " + flagColor;
+
+            //    return cardInfo;
+            //};
+
+            //string x, y, z;
+            //Console.Write("Ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti Giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz :");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            #endregion
+
+
+
+
+           
+            Console.Read();
+        }
+    }
+}
